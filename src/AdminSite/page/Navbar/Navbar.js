2d9cdcd217db4965aa172eee2da362a73de79dad
SukhrobWeb <83048281+astonaqulovsukhrob@users.noreply.github.com>
2022-04-30 18:15:24 +0500

    client page

--- src/AdminSite/page/Navbar/Navbar.js
+++ src/AdminSite/page/Navbar/Navbar.js
@@ -1,8 +1,8 @@
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useLocation } from "react-router-dom";
-import { LeftIcon, LogoAdmin, Seacrch } from "../../../assates/icons/Icons";
+import { LogoAdmin, Seacrch } from "../../../assates/icons/Icons";
 import adminLogo from "../../../assates/images/Ellipse3.png";
-import { setActive, setCurrenPage } from "../../../Redux/stored_reducer";
+import { setCurrenPage } from "../../../Redux/stored_reducer";
 import "../unversal.scss";
 import { AllPages } from "../../grouping/grouping";
 import SeacrchIcon from "../../Components/searchIcon/SeacrchIcon";
@@ -11,13 +11,10 @@ import { useState } from "react";
 export const Navbar = () => {
   const { pathname } = useLocation();
   const dispatch = useDispatch();
-  const [active, setActive] = useState(true);
-
-  const handleLeft = () => {};
 
   return (
     <>
-      <div className="navbar-top">
+      <div className="navbar-top ">
         <div className="navbar_content">
           <div>
             <LogoAdmin />
@@ -31,15 +28,13 @@ export const Navbar = () => {
             <h6>Admin</h6>
           </div>
         </div>
-        <div className="navbar_left">
+        <div className="navbar_left ">
           {AllPages?.map((page, i) => {
             page?.path === pathname && dispatch(setCurrenPage(page));
             return (
               <Link
                 to={page.path}
-                className={`nav-link ${
-                  pathname === page.path ? "activee" : ""
-                }`}
+                className={`nav-link ${pathname === page.path ? "active" : ""}`}
               >
                 <span className="me-1 seact_icon">
                   <SeacrchIcon icon={page?.icon} />
@@ -48,9 +43,6 @@ export const Navbar = () => {
               </Link>
             );
           })}
-          <span className="left-button" onClick={handleLeft}>
-            <LeftIcon />
-          </span>
         </div>
       </div>
     </>
