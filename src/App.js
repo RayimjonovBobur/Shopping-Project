a7cb3ce2e86a6ec199d314ac649f0919cd774e4c
SukhrobWeb <83048281+astonaqulovsukhrob@users.noreply.github.com>
2022-05-10 13:41:11 +0500

    z

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
