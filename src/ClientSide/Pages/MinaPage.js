3a1caebe99cd7c2860512c1a72ce2f1c3b8867f3
BoburDev <86083338+RayimjonovBobur@users.noreply.github.com>
2022-05-11 16:37:15 +0500

    Cart changed

--- src/ClientSide/Pages/MinaPage.js
+++ src/ClientSide/Pages/MinaPage.js
@@ -5,13 +5,15 @@ import FilterData from "../Components/FilterData/FilterData";
 import Footer from "../Components/Footer/Footer";
 import Header1 from "../Components/Header/Header1";
 import HeaderShop from "../Components/Header/HeaderShop";
+import Reference from "../Components/Reference/Reference";
 
 function MinaPage() {
   return (
     <>
       {/* <Header1 /> */}
       <HeaderShop />
-      <FilterData />
+      <Reference />
+      {/* <FilterData /> */}
       <CardShop />
       {/* <Card /> */}
       <Footer />
