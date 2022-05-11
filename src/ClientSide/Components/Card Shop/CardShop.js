2438aea3abfa1a4f0fdbb5223422867e03440863
BoburDev <86083338+RayimjonovBobur@users.noreply.github.com>
2022-05-11 12:18:30 +0500

    Car chnaged

--- src/ClientSide/Components/Card Shop/CardShop.js
+++ src/ClientSide/Components/Card Shop/CardShop.js
@@ -3,6 +3,7 @@ import "../../../App.scss";
 import { useDispatch, useSelector } from "react-redux";
 import { setProduct } from "../../../Redux/stored_reducer";
 import img1 from "../../../assates/images/img1.png";
+import { Combined, Combined2 } from "../../../assates/icons/Icons";
 
 const CardShop = () => {
   const { product } = useSelector((state) => state?.users_reducer);
@@ -28,15 +29,114 @@ const CardShop = () => {
             <div className="card-body">
               <img className="card-img" src={img1} alt="" />
             </div>
-            <ul
-              class="list-group list-group-flush"
-              style={{ marginLeft: "20px" }}
-            >
+            <ul class="list-group list-group-flush">
               <h5 class="card-title">Advan RT whel R18</h5>
               <p className="category-type">G’ildiraklar bo’limi</p>
-              h6.
+              <h6 className="card-price">47 450 000 cўм</h6>
+              <div className="combined">
+                <li>
+                  <Combined />
+                </li>
+                <li>
+                  <Combined />
+                </li>{" "}
+                <li>
+                  <Combined />
+                </li>{" "}
+                <li>
+                  <Combined />
+                </li>{" "}
+                <li>
+                  <Combined2 />
+                </li>
+              </div>
+              <button className=" card-more-btn">Batafsil</button>
             </ul>
           </div>{" "}
+          <div class="card-shop">
+            <div className="card-body">
+              <img className="card-img" src={img1} alt="" />
+            </div>
+            <ul class="list-group list-group-flush">
+              <h5 class="card-title">Advan RT whel R18</h5>
+              <p className="category-type">G’ildiraklar bo’limi</p>
+              <h6 className="card-price">47 450 000 cўм</h6>
+              <div className="combined">
+                <li>
+                  <Combined />
+                </li>
+                <li>
+                  <Combined />
+                </li>{" "}
+                <li>
+                  <Combined />
+                </li>{" "}
+                <li>
+                  <Combined />
+                </li>{" "}
+                <li>
+                  <Combined2 />
+                </li>
+              </div>
+              <button className=" card-more-btn">Batafsil</button>
+            </ul>
+          </div>{" "}
+          <div class="card-shop">
+            <div className="card-body">
+              <img className="card-img" src={img1} alt="" />
+            </div>
+            <ul class="list-group list-group-flush">
+              <h5 class="card-title">Advan RT whel R18</h5>
+              <p className="category-type">G’ildiraklar bo’limi</p>
+              <h6 className="card-price">47 450 000 cўм</h6>
+              <div className="combined">
+                <li>
+                  <Combined />
+                </li>
+                <li>
+                  <Combined />
+                </li>{" "}
+                <li>
+                  <Combined />
+                </li>{" "}
+                <li>
+                  <Combined />
+                </li>{" "}
+                <li>
+                  <Combined2 />
+                </li>
+              </div>
+              <button className=" card-more-btn">Batafsil</button>
+            </ul>
+          </div>{" "}
+          <div class="card-shop">
+            <div className="card-body">
+              <img className="card-img" src={img1} alt="" />
+            </div>
+            <ul class="list-group list-group-flush">
+              <h5 class="card-title">Advan RT whel R18</h5>
+              <p className="category-type">G’ildiraklar bo’limi</p>
+              <h6 className="card-price">47 450 000 cўм</h6>
+              <div className="combined">
+                <li>
+                  <Combined />
+                </li>
+                <li>
+                  <Combined />
+                </li>{" "}
+                <li>
+                  <Combined />
+                </li>{" "}
+                <li>
+                  <Combined />
+                </li>{" "}
+                <li>
+                  <Combined2 />
+                </li>
+              </div>
+              <button className=" card-more-btn">Batafsil</button>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
