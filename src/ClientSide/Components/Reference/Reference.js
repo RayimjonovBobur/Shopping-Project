334d543c6902fdfbbd8fd746efb31baca568185f
BoburDev <86083338+RayimjonovBobur@users.noreply.github.com>
2022-05-11 12:29:20 +0500

    sdfd

---
+++ src/ClientSide/Components/Reference/Reference.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Reference = () => {
+    return (
+        <div>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-6"></div>
+                    <div className="col-md-6"></div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Reference;
