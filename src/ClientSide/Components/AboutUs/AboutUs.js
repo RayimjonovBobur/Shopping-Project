158f655294ec6065c66681aba45ec94a89985b86
SukhrobWeb <83048281+astonaqulovsukhrob@users.noreply.github.com>
2022-05-13 21:36:24 +0500

    footer

---
+++ src/ClientSide/Components/AboutUs/AboutUs.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Footer from "../Footer/Footer";
+import Header from "../Header/Header";
+
+function AboutUs() {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+}
+
+export default AboutUs;
