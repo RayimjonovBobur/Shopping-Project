2e4753d1ec4f767de68f5835800917aa666bc5f7
BoburDev <86083338+RayimjonovBobur@users.noreply.github.com>
2022-05-16 17:22:28 +0500

    Image carusel

--- src/ClientSide/Components/Basket/Basket.js
+++ src/ClientSide/Components/Basket/Basket.js
@@ -1,49 +1,9 @@
-import React, { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Delete } from "../../../assates/icons/Icons";
-import { setDelete } from "../../../Redux/stored_reducer";
-import { FullIcon, NewIcon1 } from "../../utilities/icons";
-import ImageCr from "../Carusel/ImageCr";
+import React from "react";
 import "./basket.scss";
 
 const Basket = () => {
-  const { active, product } = useSelector((state) => state?.users_reducer);
-  const [count, setCount] = useState(1);
-  const [price, setPrice] = useState(100);
-  const [activePrice, setActivePrice] = useState(100);
-  const dispatch = useDispatch();
-
-  let incrementCount = () => {
-    if (count < 5) {
-      setCount(count + 1);
-      setActivePrice(activePrice + price);
-    }
-  };
-
-  let decrementCount = () => {
-    if (count > 1) {
-      setCount(count - 1);
-      setActivePrice(activePrice - price);
-    }
-  };
-
-  const handleDelete = (item) => {
-    dispatch(setDelete(item));
-  };
-
   return (
     <div className="modal_client">
-      <button
-        type="button"
-        class="basket-btn btnSavatcha"
-        data-bs-toggle="modal"
-        data-bs-target="#staticBackdrop"
-      >
-        <span className="slider-icon">
-          <FullIcon />
-        </span>
-      </button>
-
       <div
         class="modal fade"
         id="staticBackdrop"
